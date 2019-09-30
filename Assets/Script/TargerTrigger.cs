57246ceef57a18a442189f18b34f1b3eaacb04ce
B533P-12\533 <loucaluc@live.fr>
2019-09-30 15:47:12 -0400

    Fin cours12

---
+++ Assets/Script/TargerTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargerTrigger : MonoBehaviour {
+
+    // Use this for initialization
+    AudioSource sound;
+    void Start()
+    {
+        sound = FindObjectOfType<AudioSource>();
+    }
+    public void OnTriggerEnter (Collider collider)
+    {
+        sound.Play();
+    }
+}
